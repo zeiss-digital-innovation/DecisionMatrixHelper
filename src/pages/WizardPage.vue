ad4f7a76015b8d1bbea0d9d8db94f9b544346282
Martin Freiberger <martin.freiberger@zeiss.com>
2022-12-05 21:31:01 +0100

    added decision matrix component

--- src/pages/WizardPage.vue
+++ src/pages/WizardPage.vue
@@ -1,6 +1,6 @@
 <template>
   <q-page>
-    <div class="q-ma-xl">
+    <div class="q-ma-lg">
       <q-dialog v-model="showConfirmationDialog" persistent>
         <q-card>
           <q-card-section class="row items-center bg-warning text-white">
@@ -29,77 +29,7 @@
           icon="settings"
           :done="step > 1"
         >
-          <div class="q-gutter-md">
-            <q-list class="col-8">
-              <q-item
-                v-for="feature in featureStore.features"
-                :key="feature.id"
-                clickable
-                v-ripple
-                @click="handleItemClicked(feature)"
-              >
-                <q-item-section>
-                  <q-item-label>{{ feature.name }}</q-item-label>
-                  <q-item-label caption lines="2">{{
-                    feature.description
-                  }}</q-item-label>
-                </q-item-section>
-
-                <q-item-section side top>
-                  <q-item-label caption>{{
-                    Status[feature.status]
-                  }}</q-item-label>
-                  <q-icon
-                    v-if="feature.isExclusive === true"
-                    name="priority_high"
-                    color="accent"
-                  ></q-icon>
-                </q-item-section>
-              </q-item>
-
-              <q-separator spaced inset />
-            </q-list>
-            <div class="col-5">
-              <FeatureForm v-model:id="featureStore.currentFeature.id" />
-            </div>
-          </div>
-          <q-dialog
-            v-model="showDetails"
-            @hide="
-              featureStore.currentFeature = {
-                id: uuid(),
-                name: '',
-                description: '',
-                status: Status.undefined,
-                isExclusive: true,
-              }
-            "
-          >
-            <q-card style="min-width: 50vw">
-              <q-card-section
-                ><FeatureForm v-model:id="featureStore.currentFeature.id"
-              /></q-card-section>
-
-              <q-separator />
-
-              <q-card-actions align="right">
-                <q-btn
-                  v-close-popup
-                  flat
-                  color="warning"
-                  label="Delete"
-                  @click="showConfirmationDialog = true"
-                />
-                <q-btn
-                  v-close-popup
-                  flat
-                  color="primary"
-                  label="Save"
-                  @click="handleSave"
-                />
-              </q-card-actions>
-            </q-card>
-          </q-dialog>
+          <FeaturesComponent />
         </q-step>
 
         <q-step
@@ -109,49 +39,27 @@
           icon="create_new_folder"
           :done="step > 2"
         >
-          <div class="q-gutter-y-md column">
-            <q-list class="col-8">
-              <q-item
-                v-for="tool in toolStore.tools"
-                :key="tool.id"
-                clickable
-                v-ripple
-              >
-                <q-item-section>
-                  <q-item-label>{{ tool.name }}</q-item-label>
-                  <q-item-label caption lines="2">{{
-                    tool.description
-                  }}</q-item-label>
-                </q-item-section>
-              </q-item>
-
-              <q-separator spaced inset />
-            </q-list>
-            <BasicForm v-model:id="toolStore.currentAlternative.id" />
-          </div>
+          <AlternativesComponent />
         </q-step>
 
         <q-step :name="3" title="Decision Matrix" icon="add_comment">
-          <q-table
-            title="Decision Matrix"
-            :rows="featureStore.features"
-            row-key="featureStore.features.id"
-          />
+          <DecisionMatrixComponentVue />
         </q-step>
 
         <template v-slot:navigation>
           <q-stepper-navigation>
+            <q-space />
             <q-btn
               @click="$refs.stepper.next()"
               color="primary"
               :label="step === 3 ? 'Finish' : 'Continue'"
             ></q-btn>
-            <q-btn
+            <!-- <q-btn
               v-if="step < 3"
               label="Add"
               class="q-ml-sm"
               @click="handleAddFeature(featureStore.currentFeature)"
-            ></q-btn>
+            ></q-btn> -->
             <q-btn
               v-if="step > 1"
               flat
@@ -164,9 +72,6 @@
         </template>
       </q-stepper>
     </div>
-    <pre>
-      {{ featureStore.currentFeature }}
-    </pre>
   </q-page>
 </template>
 
@@ -175,15 +80,17 @@ import { v4 as uuid } from 'uuid';
 import { Feature } from 'src/components/models';
 import { Status } from 'src/components/StatusEnum';
 import { useFeaturesStore } from 'stores/features';
-import FeatureForm from 'components/FeatureForm.vue';
-import BasicForm from 'components/BasicForm.vue';
+import FeaturesComponent from 'src/components/FeaturesComponent.vue';
+import AlternativesComponent from 'src/components/AlternativesComponent.vue';
 import { ref } from 'vue';
-import { useToolsStore } from 'src/stores/tools';
+import { useAlternativesStore } from 'src/stores/alternative';
+import DecisionMatrixComponentVue from 'src/components/DecisionMatrixComponent.vue';
 
 const featureStore = useFeaturesStore();
-const toolStore = useToolsStore();
+const alternativeStore = useAlternativesStore();
 const step = ref(1);
 const showDetails = ref(false);
+const features = ref([]);
 
 const status = Object.values(Status).filter((item) =>
   isNaN(parseInt(item.toString()))
