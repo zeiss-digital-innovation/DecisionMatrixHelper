d73f22dfebad9e3ebe2ca95c287bc3021396af13
Martin Freiberger <martin.freiberger@zeiss.com>
2023-01-21 14:23:15 +0100

    fixes #3

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,20 +1,21 @@
 <template>
   <q-page class="flex justify-center items-center">
     <div class="q-pa-md row items-start q-gutter-md justify-evenly">
-      <q-card style="width: 220px" v-for="card in cards" :key="card.route">
+      <q-card style="width: 270px" v-for="card in cards" :key="card.route">
         <q-card-section avatar class="q-ml-lg q-mt-lg" horizontal>
           <q-icon size="md" :name="card.icon" color="secondary" />
         </q-card-section>
         <q-card-section>
           <div class="text-h6">{{ card.title }}</div>
-          <div class="text-subtitle2">{{ card.subtitle }}</div>
+          <div class="text-subtitle2 text-accent">{{ card.subtitle }}</div>
         </q-card-section>
 
         <q-card-actions>
           <q-btn
             flat
             color="primary"
-            :label="'go to: ' + card.title"
+            icon="arrow_forward_ios"
+            :label="card.title"
             :to="card.route"
             no-caps
           />
@@ -22,13 +23,12 @@
           <q-space />
 
           <q-btn
-            color="grey"
+            color="accent"
             round
+            size="md"
             flat
             dense
-            :icon="
-              card.expanded.value ? 'keyboard_arrow_up' : 'keyboard_arrow_down'
-            "
+            :icon="card.expanded.value ? 'keyboard_arrow_up' : 'help'"
             @click="card.expanded.value = !card.expanded.value"
           />
         </q-card-actions>
@@ -52,7 +52,7 @@ const cards = [
     title: 'Wizard',
     subtitle: 'guided decision making',
     description:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis voluptatibus, molestias dolores voluptas dicta iure illo. Vero voluptate velit aperiam praesentium accusantium unde esse? Sunt earum quas non excepturi sit.',
+      'The wizard will guide you during the decision making process. First you will add features, comparable properties, that you want to focus on. Afterwards you will be asked to add the alternatives you want to decide on. When you added at least two alternatives you can step further to decide on the given alternatives and features. In this step you need to assess each feature per alternative. The higher you assess a feature, that was already marked as important, the higher this decision will weight.',
     route: 'wizard',
     icon: 'auto_fix_normal',
     expanded: ref(false),
@@ -61,7 +61,7 @@ const cards = [
     title: 'Alternatives',
     subtitle: 'edit all alternatives',
     description:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis voluptatibus, molestias dolores voluptas dicta iure illo. Vero voluptate velit aperiam praesentium accusantium unde esse? Sunt earum quas non excepturi sit.',
+      'With alternatives you describe the entities that you are trying to distinguish. Each alternative will be compared by the features you provide. Depending on the priorities of your features and the assessment per alternative you will generate a score that enables you to compare each given alternative.',
     route: 'alternatives',
     icon: 'build',
     expanded: ref(false),
@@ -70,16 +70,16 @@ const cards = [
     title: 'Features',
     subtitle: 'edit all features',
     description:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis voluptatibus, molestias dolores voluptas dicta iure illo. Vero voluptate velit aperiam praesentium accusantium unde esse? Sunt earum quas non excepturi sit.',
+      'Features are defined as comparable properties of each Alternative. It will make more sense, if you choose features that are within the domain of the alternatives you are trying to compare against each other. With a given feature you need to determine their importance for you. Marking features as exclusive will automatically mark them as the most important feature. You can have more than one exclusive feature.',
     route: 'features',
     icon: 'checklist',
     expanded: ref(false),
   },
   {
     title: 'Decision Matrix',
-    subtitle: 'show your decisions',
+    subtitle: 'evaluate each alternative per feature',
     description:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis voluptatibus, molestias dolores voluptas dicta iure illo. Vero voluptate velit aperiam praesentium accusantium unde esse? Sunt earum quas non excepturi sit.',
+      'After you added at least one alternative and one feature the decision matrix will be available. With the matrix you have the opportunity to evaluate each feature per alternative. This assessment will be multiplied with the importance of that particular feature. For each feature given, all scores will be summed up. The resulting score will the base for the comparison. ',
     route: 'decisionMatrix',
     icon: 'functions',
     expanded: ref(false),
